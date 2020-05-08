e851d29905ac41500f60d5ef62365167bf71ad94
Nicholas Oliveira <nicholas@atomocom.com.br>
2020-05-08 16:03:18 -0300

    subindo update 0805-001

--- js/lib.js
+++ js/lib.js
@@ -1,1 +1,36 @@
 //Lib JS
+// Basic
+$(document).ready(function () {
+  $(document).on("scroll", onScroll);
+
+  $('.navbar-nav a[href^="#"]').on('click', function (e) {
+    e.preventDefault();
+    $('.navbar-nav li a').removeClass('active');
+    $(this).addClass('active');
+    var id = $(this).attr('href'),
+      targetOffset = $(id).offset().top;
+
+    $('html, body').animate({
+      scrollTop: targetOffset - 80
+    }, 500);
+  });
+
+});
+
+// Use Your Class or ID For Selection 
+function onScroll(event) {
+  var scrollPos = $(document).scrollTop();
+  $('.navbar-nav li a').each(function () {
+    var currLink = $(this);
+    var refElement = $(currLink.attr("href"));
+    if (refElement.position() != undefined) {
+      if (refElement.position().top - 80 <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+        $('.navbar-nav li a').removeClass("active");
+        currLink.addClass("active");
+      }
+      else {
+        currLink.removeClass("active");
+      }
+    }
+  });
+}
